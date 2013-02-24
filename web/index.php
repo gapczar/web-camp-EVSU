11d33f3af8866d4c8597dc99db5f31992aa52785
GAPLabs <GAPLabs@missu-poll.local>
2013-02-24 12:50:33 +0800

    fer

--- web/index.php
+++ web/index.php
@@ -19,31 +19,35 @@ $markup=new markup();
 
 <body>
 	<div id="wrapper">
-		<header>
-			<h1 class="text-glow-large">EVSU</h1>
-			<nav>
-				<ul>
-					<li><a class="text-glow-small" href="">Home</a></li>
-					<li><a class="text-glow-small" href="">Vote</a></li>
-					<li><a class="text-glow-small" href="">Profile</a></li>
-					<li><a class="text-glow-small" href="">Log Out</a></li>
-				</ul>
-			</nav>
-		</header>
+		<div id="banner-outer-wrapper">
+			<div id="banner-inner-wrapper">
+				<div id="banner">
+					<h1 class="text-glow-large">EVSU</h1>
+				</div>
+			</div>
+		</div>
+		<div id="menu-outer-wrapper">
+			<div id="menu-inner-wrapper">
+				<div id="menu">
+					<ul>
+						<li><a class="text-glow-small" href="">Home</a></li>
+						<li><a class="text-glow-small" href="">Vote</a></li>
+						<li><a class="text-glow-small" href="">Profile</a></li>
+						<li><a class="text-glow-small" href="">Log Out</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+			
+	<div id="content-side-outer-wrapper">
+		<div id="content-side-inner-wrapper">
+			<div id="content-wrapper">
+				<div id="content">
+				</div>
+			</div>
 
-	<div id="content">
-		<section>
-
-			<article>
-
-
-			</article>
-
-
-		</section>	
-			<aside>
-	
-					<article>
+			<div id="side-wrapper">
+				<div id="side">
 						<form action="" method="">
 						<?php
 							$markup->add_input('text','username','','placeholder="username" required');
@@ -51,9 +55,15 @@ $markup=new markup();
 							$markup->add_input('submit','login','value="Login"');
 						?>
 						</form>
-					</article>
-			</aside>
+				</div>
+			</div>
 		</div>
 	</div>
+	<div id="footer-outer-wrapper">
+		<div id="footer-inner-wrapper">
+			<div id="footer"></div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
