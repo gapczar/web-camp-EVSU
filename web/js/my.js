8885ac799b9d6b984b2b93274f3501dd548e5d80
GAPLabs <GAPLabs@noel.goabroadmobile.com>
2013-02-24 13:38:26 +0800

    fdfd

--- web/js/my.js
+++ web/js/my.js
@@ -1,12 +1,22 @@
 $(document).ready({
 	
+	var imgsrc;
+
 	//.....slide
 	function slide(){
-		$("#slider-wrap img:first").fadeOut(function(){
-			$(this).appendTo($("#slider-wrap")).fadeIn(2000);
+		$("#slider img:first").fadeOut(function(){
+			$(this).appendTo($("#slider")).fadeIn(2000);
 		});
 	}
 	setInterval(function(){slide()},3000);
 
-	
+	$(".category").click(function(){
+
+		imgsrc = $(this).children(img).atttr("src");
+
+		$("#zoom img").attr("src",imgsrc);
+
+		$("#zoom").css({"display":"block","height":"30px","width":"30px"})
+					.animate({"width":"600px","height","400px"},"slow");
+	});
 });
